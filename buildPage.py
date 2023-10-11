b58e02f3eb7f96b2b3767a670f8fa965577dc5db
silvup <maks990802@gmail.com>
2023-10-11 13:50:05 +0200

    Next steps

--- buildPage.py
+++ buildPage.py
@@ -1,6 +1,8 @@
 from getDragonNumbers import *
 from getPentakillInfo import *
 
+mapping_table = str.maketrans({'{': '','}': '','\'': ''})
+
 with open("index.html","w") as f:
 	f.writelines(["<!DOCTYPE html>\n",
 		"<html>\n",
@@ -10,7 +12,7 @@ with open("index.html","w") as f:
 		"</head>\n",
 		"<body>\n",
 		"<h1>PICKEM STATS</h1>\n\n",
-		"<h2 class=lightgreen>Longest game: ", longest_gametime,  "</h2>\n", "\n<h2 class=lightgreen>Dragon Count</h2>", "</h2>\n"])	
+		"<h2 class=lightgreen>Longest game: ", longest_gametime, " ", longest_game,   "</h2>\n", "\n<h2 class=lightgreen>Dragon Count</h2>", "</h2>\n"])	
 	f.write("<p class=lightgreen>Cloud: " + str(cloud) +"</p>\n")
 	f.write("<p class=lightgreen>Infernal: " + str(infernal) + "</p>\n")
 	f.write("<p class=lightgreen>Ocean: " + str(ocean) + "</p>\n")
@@ -20,6 +22,9 @@ with open("index.html","w") as f:
 	f.write("<h2 class=lightgreen>Reverse sweeps: 0</h2>\n")
 	f.write("<h2 class=lightgreen>Baron steals: 0</h2>\n")
 	f.write("<h2 class=lightgreen>Pentakills: " + str(pentakillNumber) + " " + pentakillNames + "</h2>\n")
-	f.write("<h2 class=lightblue>Most baron steals: ?</h2>\n")
+	f.write("<h2 class=lightsalmon>Most firstbloods: </h2>\n")
+	f.write("<p class=lightsalmon>" + str(count_firstbloods).translate(mapping_table) + "</p>\n")
+	f.write("<h2 class=lightsalmon>Most baron steals: ?</h2>\n")
 	f.write("<h2 class=lightblue>Best 2 seeds region: ?</h2>\n")
+	f.write("<h2 class=lightblue>Worlds winners: ?</h2>\n")
 	f.writelines(["</body>\n","</html>"])
--- getDragonNumbers.py
+++ getDragonNumbers.py
@@ -1,5 +1,7 @@
 import urllib.request
 import io
+from bs4 import BeautifulSoup
+from collections import Counter
 
 class AppURLopener(urllib.request.FancyURLopener):
     version = "Mozilla/5.0"
@@ -7,7 +9,11 @@ class AppURLopener(urllib.request.FancyURLopener):
 opener = AppURLopener()
 
 longest_gametime = "00:00"
+shortest_gametime = "99:99"
+longest_game = ""
 keyWord = "Game Time"
+vs = "vs"
+
 cloud = 0
 infernal = 0
 ocean = 0
@@ -16,6 +22,35 @@ mountain = 0
 hextech = 0
 
 file = ""
+
+firstblood_kill = []
+count_firstbloods = dict()
+
+def get_firstblood(urlD):
+	buf = io.StringIO(urlD)
+	item = ""
+	events_found = False
+	inside_tab = False
+	td_count = 0
+	td_found = False
+	current_player_name = "Noone"
+	while item.strip() != "</html>":
+		item = buf.readline()
+		if inside_tab and "</td>" in item.strip():
+			td_found = False
+			td_count = td_count % 7	
+		if td_found:
+			if td_count == 3:
+				current_player_name = item.strip()
+			if td_count == 5 and "kill-icon" in item.strip():
+				return current_player_name
+		if "Events" in item.strip():
+			events_found = True
+		if events_found and "Action" in item.strip():
+			inside_tab = True
+		if inside_tab and "<td" in item.strip():
+			td_found = True
+			td_count += 1
 
 def get_gametime(urlD):
 	gametime_found = False
@@ -28,17 +63,32 @@ def get_gametime(urlD):
 		if keyWord in item:
 			gametime_found = True
 	return "00:00"
-	
+
+def get_gamename(urlD):
+	buf = io.StringIO(urlD)
+	item = ""
+	gamename = ""
+	while item.strip() != "</html>":
+		item = buf.readline()
+		if vs in item.strip():
+			return item.strip()[41:-72]
 
 with open("games_timelines.txt") as file:
 	for item in file:
 		resp = opener.open(item)
 		urlB = resp.read()
 		urlD = urlB.decode("utf8")
+		urlPretty = BeautifulSoup(urlB).prettify()
+		gamename = get_gamename(urlD)
 		current_gametime = get_gametime(urlD)
-		print(current_gametime)
+		firstblood_kill.append(get_firstblood(urlPretty))
+		count_firstbloods = dict(Counter(firstblood_kill))
+		print(count_firstbloods)
 		if int(longest_gametime.replace(':', '')) < int(current_gametime.replace(':','')):
-			longest_gametime = current_gametime 	
+			longest_gametime = current_gametime
+			longest_game = gamename
+		if int(shortest_gametime.replace(':', '')) > int(current_gametime.replace(':','')):
+			shortest_gametime = current_gametime	
 		cloud += urlD.count("cloud-dragon")
 		infernal += urlD.count("fire-dragon")
 		ocean += urlD.count("ocean-dragon")
